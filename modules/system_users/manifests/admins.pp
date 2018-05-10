f100503c0a4ecd56f0e02d2eac301223bf005632
Vincent <vinhmac@hotmail.com>
2018-05-10 14:24:47 +0000

    Add if else changes

--- modules/system_users/manifests/admins.pp
+++ modules/system_users/manifests/admins.pp
@@ -1,16 +1,26 @@
 class system_users::admins {
 
-  package { 'csh':
-  ensure=> latest,
-  }
   group { 'staff':
     ensure => present,
   }
 
+  if $facts['kernel'] == 'windows' {
+    user { 'admin':
+      group => 'staff',
+    }
+  }
+
+  else {
+    package {
+      'csh':
+      ensure => latest,
+    }
+
   user { 'admin':
-    group => 'staff',
-    shell => '/bin/csh',
+    group   => 'staff',
+    shell   => '/bin/csh',
     require => Package['csh']
   }
+}
 
 }
